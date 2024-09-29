0aea5d02d2208d7def6280fdcd5544c7c0c03d0f
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-09-29 12:25:42 +0200

    Passes all tests

--- src/app.rs
+++ src/app.rs
@@ -1,26 +1,16 @@
 use bevy::prelude::*;
 
-use bevy::prelude::States;
 
 #[derive(Resource)]
-pub struct GoalsReached {
-    bonus: u32,
-}
-
-// custom implementation for unusual values
-impl Default for GoalsReached {
-    fn default() -> Self {
-        GoalsReached {
-            bonus: 0
-        }
-    }
+pub struct MyResource {
+    my_value: i32,
 }
 
 pub fn create_default_app() -> App {
     create_app(0)
 }
 
-pub fn create_app(initial_bonus: u32) -> App {
+pub fn create_app(initial_my_value: i32) -> App {
     let mut app = App::new();
 
     // Only add these plugin in testing.
@@ -31,7 +21,8 @@ pub fn create_app(initial_bonus: u32) -> App {
     } else {
         app.add_plugins(DefaultPlugins);
     }
-    app.init_resource::<GoalsReached>();
+
+    app.insert_resource(MyResource { my_value: initial_my_value} );
 
 
     // NO! Do not update!
@@ -43,39 +34,32 @@ pub fn create_app(initial_bonus: u32) -> App {
 
 
 #[cfg(test)]
-fn get_bonus(app: &mut App) -> u32 {
-    app.world().resource::<GoalsReached>().get().clone().bonus
+fn get_my_value(app: &mut App) -> i32 {
+    app.world().resource::<MyResource>().my_value
 }
 
 
 #[cfg(test)]
 mod tests {
-    use bevy::input::keyboard::Key;
     use super::*;
 
     #[test]
     fn test_can_create_app() {
-        create_app();
+        create_default_app();
     }
 
-    #[test]
-    fn test_app_has_text() {
-        let mut app = create_app();
-        app.update();
-        assert_eq!(count_n_texts(&mut app), 1);
-    }
 
     #[test]
-    fn test_app_starts_with_no_bonus() {
+    fn test_app_starts_with_no_my_value() {
         let mut app = create_default_app();
         app.update();
-        assert_eq!(get_bonus(&mut app), 0);
+        assert_eq!(get_my_value(&mut app), 0);
     }
     #[test]
-    fn test_app_can_have_an_initial_bonus() {
-        let bonus = 42;
-        let mut app = create_app(bonus);
+    fn test_app_can_have_an_initial_my_value() {
+        let my_value = 314;
+        let mut app = create_app(my_value);
         app.update();
-        assert_eq!(get_bonus(&mut app), bonus);
+        assert_eq!(get_my_value(&mut app), my_value);
     }
 }
